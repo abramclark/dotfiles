d2b8cddf91653eb1b8dae9934fdecd887d18ff74
Abram Clark <abram@spez.local>
2017-02-11 23:53:30 -0800

    Updated dotfiles.sh to link to upstream on init

--- .shell/dotfiles.sh
+++ .shell/dotfiles.sh
@@ -1,9 +1,9 @@
 cd
-alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
 # initialize repository
-if [ ! -d .dotfiles ]; then
-  git init .dotfiles
-  dotfiles config --local status.showUntrackedFiles no
-  dotfiles remote -v add origin git@github.com:AbramClark/dotfiles.git
+if [ ! -d .git ]; then
+  git init .
+  git config --local status.showUntrackedFiles no
+  git remote -v add origin git@github.com:AbramClark/dotfiles.git
+  git push --set-upstream origin master
 fi
+cd -
