639c9bbff6f6bb3679550d9d9559245816029264
Abram Clark <abram@easypost.com>
2018-06-08 05:02:08 -0700

    Refactored prompt code in bashrc.sh, removed obsoleted 05.colors.sh

--- .bash.d/bashrc.sh
+++ .bash.d/bashrc.sh
@@ -17,12 +17,25 @@ export LESS='-R' # proper less color escape interpretation (for ipython)
 export QHOME=~/bin/q-lang/
 IFS=$(echo -e " \n	")
 
-box_name=${HOSTNAME/.*/}
+
+## set prompt
+
+function set_color {
+    echo -e "\033[38;05;${1}m"
+}
+
 function git_branch {
     b=$(git symbolic-ref HEAD 2> /dev/null);
     if [ $b ]; then echo -n "(${b##refs/heads/})"; fi
 }
-PS1="\[$prompt_color\](\w)\[$Normal\]\$(git_branch)\[$prompt_color\]@$box_name \[$Normal\]"
 
-# show prompt in red if root
-if [ "$UID" = "0" ]; then prompt_color=$Red; fi 
+function __main__ {
+    local normal=$(echo -e '\033[0;0m')
+    local box_name=${HOSTNAME/.*/}
+
+    PS1="\[$prompt_color\](\w)\[$normal\]\$(git_branch)\[$prompt_color\]@$box_name \[$normal\]"
+
+    # show prompt in red if root
+    if [ "$UID" = "0" ]; then prompt_color=$Red; fi 
+}
+__main__
