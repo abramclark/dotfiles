73bb1a0eaaa7e86669056f2409c13d8ecad5ea58
Abram Clark <abram@easypost.com>
2022-07-12 01:00:07 -0700

    Ipython config

--- .ipython/profile_default/startup/prompt.py
+++ .ipython/profile_default/startup/prompt.py
@@ -1,18 +1,16 @@
-# ~/.ipython/profile_default/startup
-from IPython import get_ipython
-from IPython.terminal.prompts import Prompts, Token
+import IPython  # awkward but let's not polute namespace
 
 
-class MyPrompt(Prompts):
+class MyPrompt(IPython.terminal.prompts.Prompts):
     def continuation_prompt_tokens(self, cli=None, width=None): return []
     def in_prompt_tokens(self, cli=None): return []
 
     def out_prompt_tokens(self):
         return [
-            (Token.OutPrompt, '#['),
-            (Token.OutPromptNum, str(self.shell.execution_count)),
-            (Token.OutPrompt, ']> '),
+            (IPython.terminal.prompts.Token.OutPrompt, '#['),
+            (IPython.terminal.prompts.Token.OutPromptNum, str(self.shell.execution_count)),
+            (IPython.terminal.prompts.Token.OutPrompt, ']> '),
         ]
 
-ip = get_ipython()
-ip.prompts = MyPrompt(ip)
+
+get_ipython().prompts = MyPrompt(get_ipython())
